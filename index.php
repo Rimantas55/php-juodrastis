d684b14df7a09212162886393abe0a2c5a16f9f0
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-05-07 00:13:02 +0300

    grikiu beveik uzbaigta uzduotis tik nemoku for output vienoje eilute padaryt

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ $size = 100;
 <html lang="en">
 <head>
     <meta charset="utf-8">
-<!--    meta tagas kuris refresina browseri, galima nurdyti kiek sec-->
-<!--    <meta http-equiv="refresh" content="1">-->
+    <!--    meta tagas kuris refresina browseri, galima nurdyti kiek sec-->
+    <!--    <meta http-equiv="refresh" content="1">-->
     <title>Php juodrastis</title>
 
     <!-- Google Fonts -->
@@ -31,6 +31,7 @@ $size = 100;
             border: 5px solid black;
 
         }
+
         /*kodel neprisitaiko paveikslelis prie div aukscio ir plocio*/
         img {
             max-width: 100%;
@@ -44,6 +45,7 @@ $size = 100;
             justify-content: flex-start;
             align-items: center;
         }
+
         .bomb {
             background-image: url("https://static2.inspektorius.lt/usi/980x654/3/99/ad/09/99ad096b1d352b45732b0918fccc2517.jpg?v=4.0.5.09&t=cr&s=980x654&m=3&f=/Uploads/UArticles/leadPhotos/e2/25/68/86/e22568862d16199d3e15b2b7c29ab390.jpg");
             background-size: cover;
@@ -59,102 +61,73 @@ $size = 100;
         }
     </style>
 </head>
-    <body>
-        <h1>1 Uzduotis - Cizu skaiciavimas</h1>
-            <?php print cizos(); ?>
+<body>
+<h1>1 Uzduotis - Cizu skaiciavimas</h1>
+<?php print cizos(); ?>
 
 
-        <h1>2 Uzduotis - Kas sekunde didejanti bomba kuri minutes pabaigoje sprogsta</h1>
-        <?php if(date("s" <= 1)): ?>
+<h1>2 Uzduotis - Kas sekunde didejanti bomba kuri minutes pabaigoje sprogsta</h1>
+<?php if (date("s" <= 1)): ?>
 
-        <div class="bomba">
-            <img src="https://static2.inspektorius.lt/usi/980x654/3/99/ad/09/99ad096b1d352b45732b0918fccc2517.jpg?v=4.0.5.09&t=cr&s=980x654&m=3&f=/Uploads/UArticles/leadPhotos/e2/25/68/86/e22568862d16199d3e15b2b7c29ab390.jpg" alt="bomba" class="tiksinti-bomba">
-
-
-        </div>
-            <h2>Time: <?php print date("s") ;?></h2>
-        <?php else : ?>
-
-            <div class="bomba">
-                <img src="https://img.newatlas.com/satan-truth-58.jpg?auto=format%2Ccompress&ch=Width%2CDPR&fit=crop&h=347&q=60&rect=0%2C42%2C1496%2C841&w=616&s=1913d819e9cfb41ded46cd560d2af6dc" alt="bomba" class="tiksinti-bomba">
-                <p>Time: <?php print date("s") ;?></p>
-            </div>
-
-        <?php endif; ?>
-
-    <h1>Tado pavyzdys</h1>
-    <section>
-<!--        kode po action nera 1 ()skaiciaus vienas kaip kad style nurodyta-->
-        <div class="bomb action-<?php print $explosion; ?>"></div>
-        <h1>: <?php print $timer; ?></h1>
-    </section>
-
-    <h1>3 Uzduotis Kokie bus orai? Spaudziant refresh keiciasi orai</h1>
+    <div class="bomba">
+        <img src="https://static2.inspektorius.lt/usi/980x654/3/99/ad/09/99ad096b1d352b45732b0918fccc2517.jpg?v=4.0.5.09&t=cr&s=980x654&m=3&f=/Uploads/UArticles/leadPhotos/e2/25/68/86/e22568862d16199d3e15b2b7c29ab390.jpg"
+             alt="bomba" class="tiksinti-bomba">
 
 
-    <h1> 4 Uzduotis - Isspausdinti su php 4 vienodus img</h1>
+    </div>
+    <h2>Time: <?php print date("s"); ?></h2>
+<?php else : ?>
 
-            <?php //print ; ?>
+    <div class="bomba">
+        <img src="https://img.newatlas.com/satan-truth-58.jpg?auto=format%2Ccompress&ch=Width%2CDPR&fit=crop&h=347&q=60&rect=0%2C42%2C1496%2C841&w=616&s=1913d819e9cfb41ded46cd560d2af6dc"
+             alt="bomba" class="tiksinti-bomba">
+        <p>Time: <?php print date("s"); ?></p>
+    </div>
 
-    <h1>5 Uzduotis - siuksliu deze</h1>
-    <pre>
-        $siukles_turis = 40; (dm3)
-        $siuksliu_turis_avg_per_day = 15;
-        $max_kaupo_turis = [rand];
-    </pre>
-    <h5>Po keliu dienu kils barnas?</h5>
-    <pre>
-        Po $dienu ([data]) pirk geliu ir sampano, jeigu nori, kad zmona siuksles pati isnestu
-    </pre>
-    <?php
-    $siukles_turis = 40; //(dm3)
+<?php endif; ?>
+
+<h1>Tado pavyzdys</h1>
+<section>
+    <!--        kode po action nera 1 ()skaiciaus vienas kaip kad style nurodyta-->
+    <div class="bomb action-<?php print $explosion; ?>"></div>
+    <h1>: <?php print $timer; ?></h1>
+</section>
+
+<h1>3 Uzduotis Kokie bus orai? Spaudziant refresh keiciasi orai</h1>
+
+
+<h1> 4 Uzduotis - Isspausdinti su php 4 vienodus img</h1>
+
+<?php //print ; ?>
+
+<h1>5 Uzduotis - siuksliu deze</h1>
+<pre>
+    $siukles_turis = 40; (dm3)
     $siuksliu_turis_avg_per_day = 15;
-    $max_kaupo_turis = rand(10,20);//cia tas kur virsus susikaups;
+    $max_kaupo_turis = [rand];
+</pre>
+<h2>Po keliu dienu siuskledeze bus pilna ir gali kilti barnis?</h2>
 
-    $dienu = round(($siukles_turis + rand(10,20)) / $siuksliu_turis_avg_per_day);
-    $laikas = date('Y-m-d', strtotime('+' . $dienu . 'days')); //"+$dienu days" antras variantas
+<?php print $tekstas; ?>
 
-
-    $tekstas = "po $dienu $laikas reikia ismesti siuskles ";
-
-    print $tekstas;
-
-    ?>
-
-        <h1>6 Uzduotis - grikiai</h1>
-        <pre>
-            $grikiai = 5000; //g
-            $per_diena = rand(200, 500); //g
-
-            Kiek dienu gales valgyti grikius?
-
-            Jei turesiu $grikiai g. grikiu, galesiu juos valgyti
-            $survive_days dienu. jei pradesiu siandien, grikiai
-            pasibaigs [data]
-        </pre>
-
-    <?php
+<h1>6 Uzduotis - grikiai</h1>
+<pre>
     $grikiai = 5000; //g
-    $per_diena = rand(1, 5); //g
-    $x = 0;
+    $per_diena = rand(200, 500); //g
 
+    Kiek dienu gales valgyti grikius?
 
-    for ($per_diena = 0; $per_diena <= 5; $per_diena++){ //$per_diena++
-        $per_diena = rand(1, 5);
-        $x++;//dienu skaicius
-        print $per_diena . " Ciklo rezultatas / ";
-        print $x . " dienos" . "<br>";
-    }
+    Jei turesiu $grikiai g. grikiu, galesiu juos valgyti
+    $survive_days dienu. jei pradesiu siandien, grikiai
+    pasibaigs [data]
+</pre>
 
-
-
-
-
-    ?>
+<?php print kiek_dienu() ?>
 
 
 
-    </body>
+
+</body>
 </html>
 
 
--- juodrastis.php
+++ juodrastis.php
@@ -2,32 +2,66 @@
 
 $days = 365;
 $savaites = 52;
-$cizos_mon_thu = rand(3,4);
-$cizos_fri = rand(10,20);
-$cizos_sat_sun = rand(1,3);
+$cizos_mon_thu = rand(3, 4);
+$cizos_fri = rand(10, 20);
+$cizos_sat_sun = rand(1, 3);
 $pakelio_kaina = 3.50;
 
 
-
-
 //kodel raso kad tokio variable nera?
-$vienos_cizos_kaina = 3.5/20;
+$vienos_cizos_kaina = 3.5 / 20;
 
 //kodel raso kad tokio variable nera?
 $total_cizu_per_sav = ($cizos_mon_thu * 4) + $cizos_fri + $cizos_sat_sun;
 
-function cizos() {
+function cizos()
+{
     for ($x = 1; $x <= 5; $x++) {
-        print $x . " Savaite " . " Testas" . "<br>" ;
+        print $x . " Savaite " . " Testas" . "<br>";
     }
 }
+
 print $total_cizu_per_sav . " Cizos per sav " . $vienos_cizos_kaina * $total_cizu_per_sav . " islaidos per sav Euru" . "<br>";
 
-
-
-
-
+//----------------------
 //didejancios bombos php
 
 $bombos_aukstis = 80 + (date("s") * 5);
 $bombos_plotis = 120 + (date("s") * 5);
+
+
+//-------------------
+//Siuksledeze
+
+$siukles_turis = 40; //(dm3)
+$siuksliu_turis_avg_per_day = 15;
+$max_kaupo_turis = rand(10, 20);//cia tas kur virsus susikaups;
+
+$dienu = round(($siukles_turis + rand(10, 20)) / $siuksliu_turis_avg_per_day);
+$laikas = date('Y-m-d', strtotime('+' . $dienu . 'days')); //"+$dienu days" antras variantas
+
+
+$tekstas = "Po $dienu dienu, data: $laikas reikia ismesti siuskles, kad nekiltu barnis su zmona ";
+
+
+
+
+//-------------------
+//grikiu skaiciavimas
+
+function kiek_dienu(){
+    // $grikiai = 5000; //g
+    $x = 0;
+
+    for ($i = 1; $i < 5000; $i++) {
+
+        $i += rand(200, 500); //g
+        $x++;//dienu skaicius
+        print $i . " Ciklo rezultatas / ";
+        print $x . " dienos" . "<br>";
+    }
+}
+
+//cia toks kazkoks gudresnis
+//    $numbers = range(1, 10);
+//    echo implode('+', $numbers) . '=' . array_sum($numbers);
